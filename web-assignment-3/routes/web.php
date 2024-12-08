d44fcdf4e4a3c679fa6d79681e9f355c842931a8
Aimen Munawar <aimenmunawarofficial@gmail.com>
2024-12-08 16:00:53 +0500

    added registeration and login

--- web-assignment-3/routes/web.php
+++ web-assignment-3/routes/web.php
@@ -1,8 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 
-Route::get('/', function () {
+Route::middleware('guest')->group(function () {
+// for registeration
+Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+Route::post('/register', [RegisteredUserController::class, 'store']);
+// for login
+Route::get('/', [AuthenticatedSessionController::class, 'create'])->name('login');
+Route::post('/', [AuthenticatedSessionController::class, 'store']);
+});
+Route::middleware('auth')->group(function () {
+Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+});
+Route::get('/home', function () {
     return view('index');
 })->name('home'); // Name the route 'home'
 
